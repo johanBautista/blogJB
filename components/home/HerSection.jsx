38e2986ef75e985c59685163eb06c76048c92c24
Johan Bautista <johan.bautista@cuatroochenta.com>
2022-12-26 01:20:39 +0100

    setup project

---
+++ components/home/HerSection.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import profileImage from '../../images/johan_img.jpg';
+import classes from './HeroSection.module.scss';
+
+function HeroSection() {
+  return (
+    <div className={classes.heroSection}>
+      <dir className={classes.profileImg}>
+        <Image src={profileImage} alt="Shaif arfan" height="200" width="200" />
+      </dir>
+      <h1 className={classes.name}>jbp_DEV</h1>
+      <p className={classes.description}>
+        En busca de la Felicidad
+      </p>
+    </div>
+  );
+}
+
+export default HeroSection;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Layout from "../components/layout/Layout";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Johan_DEV</title>
+        <meta
+          name="description"
+          content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis deserunt"
+        />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
